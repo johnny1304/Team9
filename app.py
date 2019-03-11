04a003998764ad1d433eb76a025331cddaff552b
intu1 <shivasubedi2@live.ie>
2019-03-11 20:35:46 +0000

    final commit soz

--- app.py
+++ app.py
@@ -925,11 +925,7 @@ def forgot():
             return render_template('forgot.html', form=form)
     return render_template('forgot.html', form=form)
 
-<<<<<<< HEAD
-@app.route('/link',methods=["Get","Post"])
-=======
 @app.route('/link', methods=["Get","Post"])
->>>>>>> 0a7edd15c0e84323e8f881358384e7ca8841380a
 def link():
     message="Please check your email and follow the instructions."
     return render_template("link.html",messages=message)
